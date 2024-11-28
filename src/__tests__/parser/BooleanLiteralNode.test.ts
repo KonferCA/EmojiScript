ca3c452fda4903753996eb54690c80fca1464deb
jc <46619361+juancwu@users.noreply.github.com>
2024-11-27 21:48:09 -0500

    fix boolean test names

--- src/__tests__/parser/BooleanLiteralNode.test.ts
+++ src/__tests__/parser/BooleanLiteralNode.test.ts
@@ -2,11 +2,11 @@ import { describe, it, expect } from "vitest";
 import { BooleanLiteralNode } from "@/lib/parser/nodes";
 import { ASTVisitor } from "@/lib/parser/astVisitor";
 
-describe("Test String Literal Node", () => {
+describe("Test Boolean Literal Node", () => {
     const visitor = new ASTVisitor();
     const b = true;
 
-    it("should create a new number literal node", () => {
+    it("should create a new boolean literal node", () => {
         const n = new BooleanLiteralNode(b, { line: 0, column: 0 });
         expect(n.value).toEqual(b);
         expect(n.position).toEqual({ line: 0, column: 0 });
@@ -14,7 +14,7 @@ describe("Test String Literal Node", () => {
         expect(n.debug).toBeTypeOf("function");
     });
 
-    it("should construct a string with literal string for javascript", () => {
+    it("should construct a string with literal boolean for javascript", () => {
         const n = new BooleanLiteralNode(b, { line: 0, column: 0 });
         const s = n.accept(visitor);
         expect(s).toEqual(`${b}`);
