2cc1e7a5f25533c621962087483a2a7df29d10b6
jc <46619361+juancwu@users.noreply.github.com>
2024-11-27 21:47:36 -0500

    rename identifier test names

--- src/__tests__/parser/IdentifierNode.test.ts
+++ src/__tests__/parser/IdentifierNode.test.ts
@@ -2,11 +2,11 @@ import { describe, it, expect } from "vitest";
 import { IdentifierNode } from "@/lib/parser/nodes";
 import { ASTVisitor } from "@/lib/parser/astVisitor";
 
-describe("Test String Literal Node", () => {
+describe("Test Identifier Node", () => {
     const visitor = new ASTVisitor();
     const ident = "a";
 
-    it("should create a new number literal node", () => {
+    it("should create a new identifier node", () => {
         const n = new IdentifierNode(ident, { line: 0, column: 0 });
         expect(n.name).toEqual(ident);
         expect(n.position).toEqual({ line: 0, column: 0 });
@@ -14,7 +14,7 @@ describe("Test String Literal Node", () => {
         expect(n.debug).toBeTypeOf("function");
     });
 
-    it("should construct a string with literal string for javascript", () => {
+    it("should construct an identifier string for javascript", () => {
         const n = new IdentifierNode(ident, { line: 0, column: 0 });
         const s = n.accept(visitor);
         expect(s).toEqual(ident);
