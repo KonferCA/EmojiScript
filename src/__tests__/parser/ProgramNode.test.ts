903ca5c4a70ac11ac51c4e2369e9a0a553b45c7e
jc <46619361+juancwu@users.noreply.github.com>
2024-11-29 14:56:32 -0500

    add a more complex program test

--- src/__tests__/parser/ProgramNode.test.ts
+++ src/__tests__/parser/ProgramNode.test.ts
@@ -2,12 +2,20 @@ import { describe, it, expect } from "vitest";
 import {
     ArrayLiteralNode,
     BooleanLiteralNode,
+    ExpressionNode,
+    FunctionCallNode,
+    FunctionDefinitionNode,
+    IdentifierNode,
+    IfStatementNode,
+    IOOperationNode,
     NumberLiteralNode,
     ProgramNode,
     StringLiteralNode,
+    VariableDeclarationNode,
 } from "@/lib/parser/nodes";
 import { ASTVisitor } from "@/lib/parser/astVisitor";
 import type { Node } from "@/lib/parser/types";
+import { IOEmojis, RelationalEmojis } from "@/lib/emojiConstants";
 
 describe("Test Program Node", () => {
     const visitor = new ASTVisitor();
@@ -28,6 +36,67 @@ describe("Test Program Node", () => {
         expect(n.accept).toBeTypeOf("function");
         expect(n.debug).toBeTypeOf("function");
         const expectedStr = `0;"🍦";true;[false]`;
+        const jsStr = n.accept(visitor);
+        expect(jsStr).toEqual(expectedStr);
+    });
+
+    it("should build a runnable program", () => {
+        const statements: Node[] = [
+            new FunctionDefinitionNode(
+                new IdentifierNode("max", position),
+                [
+                    new IdentifierNode("a", position),
+                    new IdentifierNode("b", position),
+                ],
+                [
+                    new IfStatementNode(
+                        new ExpressionNode(
+                            new IdentifierNode("a", position),
+                            RelationalEmojis.GREATER,
+                            new IdentifierNode("b", position),
+                            false,
+                            position
+                        ),
+                        [
+                            new IOOperationNode(
+                                IOEmojis.PRINT,
+                                new IdentifierNode("a", position),
+                                position
+                            ),
+                        ],
+                        [
+                            new IOOperationNode(
+                                IOEmojis.PRINT,
+                                new IdentifierNode("b", position),
+                                position
+                            ),
+                        ],
+                        position
+                    ),
+                ],
+                position
+            ),
+            new VariableDeclarationNode(
+                new IdentifierNode("x", position),
+                new NumberLiteralNode(1, position),
+                position
+            ),
+            new VariableDeclarationNode(
+                new IdentifierNode("y", position),
+                new NumberLiteralNode(2, position),
+                position
+            ),
+            new FunctionCallNode(
+                new IdentifierNode("max", position),
+                [
+                    new IdentifierNode("x", position),
+                    new IdentifierNode("y", position),
+                ],
+                position
+            ),
+        ];
+        const n = new ProgramNode(statements, position);
+        const expectedStr = `function max(a,b){if(a>b){window.alert(a)}else{window.alert(b)}};let x=1;let y=2;max(x,y)`;
         const jsStr = n.accept(visitor);
         expect(jsStr).toEqual(expectedStr);
     });
